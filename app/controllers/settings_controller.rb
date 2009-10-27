d3da9e778ab18ed28078ec719429ba3079a51389
Doug Alcorn <dougalcorn@gmail.com>
2009-10-27 11:40:29 -0400

    creating settings is now polymorphic with drop down selection

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,6 +1,21 @@
 class SettingsController < ApplicationController
   resource_controller
+  before_filter :define_options
 
   create.wants.html { redirect_to settings_path }
   update.wants.html { redirect_to settings_path }
+
+  private
+
+  def build_object
+    klass = params[:setting][:type].constantize rescue Setting
+    @object ||= klass.new(params[:setting])
+  end
+
+  def define_options
+    @type_options = [["String", "Setting"]]
+    ["Integer", "Float", "Boolean"].each do |name|
+      @type_options << [name, "#{name}Setting"]
+    end
+  end
 end
--- test/integration/settings_controller_test.rb
+++ test/integration/settings_controller_test.rb
@@ -4,17 +4,27 @@ class SettingsControllerTest < ActionController::IntegrationTest
 
   context "GET settings index" do
     setup do
+      @name = "new setting #{Time.now.to_i}"
       visit settings_path
     end
 
     context "creating a new setting" do
       setup do
-        @name = "new setting #{Time.now.to_i}"
         click_link "New Setting"
         fill_in "Name", :with => @name
         click_button "Save"
       end
       should_change "Setting.count", :by => 1
+    end
+
+    context "creating a boolean setting" do
+      setup do
+        click_link "New Setting"
+        fill_in "Name", :with => @name
+        select "Boolean"
+        click_button "Save"
+      end
+      should_change "BooleanSetting.count", :by => 1
     end
   end
 end
